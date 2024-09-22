022c283299dab4837963b32c50911a585840c9bf
jasonpial <jumadavidpius@gmail.com>
2024-09-22 14:48:38 +0300

    Add files via upload

---
+++ leapyear.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool is_leap_year(unsigned int Y) {
+    
+    if (Y % 400 == 0) {
+        return true;
+    }
+    else if (Y % 100 == 0) {
+        return false;
+    }
+   else if (Y % 4 == 0) {
+        return true;
+    }
+  
+    else {
+        return false;
+    }
+}
+
+int main() {
+    unsigned int year;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (is_leap_year(year)) {
+        cout << year << " is a leap year." << '\n';
+    } else {
+        cout << year << " is not a leap year." << '\n';
+    }
+
+    return 0;
+}
+
+
